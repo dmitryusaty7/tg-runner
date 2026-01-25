21c7fe5d039e63dc81f718ae7e2921594baec0f9
dus_fetch_data <68933518+dmitryusaty7@users.noreply.github.com>
2026-01-26 03:41:59 +1000

    feat: runner skeleton

---
+++ src/config/gameConfig.js
@@ -0,0 +1,31 @@
+export const WIDTH = 540;
+export const HEIGHT = 960;
+
+export const BASE_SPEED = 500;
+export const SPEED_RAMP = 10;
+
+export const GRAVITY_Y = 1400;
+export const JUMP_VELOCITY = -620;
+
+export const SPAWN_GROUND_MS = 1200;
+export const SPAWN_FLY_MS = 2200;
+
+export const GROUND_THICKNESS = 80;
+export const GROUND_Y = 820;
+
+export const PLAYER_W = 64;
+export const PLAYER_H = 96;
+export const PLAYER_X = 120;
+export const PLAYER_Y = GROUND_Y - PLAYER_H / 2;
+
+export const ROCK_SMALL_W = 44;
+export const ROCK_SMALL_H = 32;
+export const ROCK_BIG_W = 78;
+export const ROCK_BIG_H = 56;
+
+export const CRATER_W = 120;
+export const CRATER_DEPTH = 70;
+
+export const METEOR_W = 56;
+export const METEOR_H = 28;
+export const METEOR_Y_LEVELS = [520, 590, 660];
