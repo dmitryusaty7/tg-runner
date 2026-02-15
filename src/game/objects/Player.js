08621b367e777a1c5118c62db9fd103c2b017769
dus_fetch_data <68933518+dmitryusaty7@users.noreply.github.com>
2026-02-16 04:39:31 +1000

    Добавить единый реестр ассетов и fallback для отсутствующих изображений

--- src/game/objects/Player.js
+++ src/game/objects/Player.js
@@ -1,4 +1,5 @@
 import { DEPTHS, GRAVITY_Y, JUMP_VELOCITY, PLAYER_H, PLAYER_W } from '../../config/gameConfig';
+import { ASSET_BY_ID } from '../../config/assetManifest';
 
 export class Player
 {
@@ -8,12 +9,35 @@ export class Player
         this.state = 'run';
         this.isFalling = false;
 
-        this.sprite = scene.add.rectangle(x, y, PLAYER_W, PLAYER_H, 0x4fd1c5);
-        scene.physics.add.existing(this.sprite);
+        const runTexture = this.ensurePlayerTexture('player.run', 'player-run-fallback', '#4fd1c5');
+        this.sprite = scene.physics.add.image(x, y, runTexture);
+        this.sprite.setDisplaySize(PLAYER_W, PLAYER_H);
         this.sprite.setDepth(DEPTHS.PLAYER);
         this.sprite.body.setCollideWorldBounds(true);
         this.sprite.body.setGravityY(GRAVITY_Y);
         this.sprite.body.setSize(PLAYER_W, PLAYER_H, true);
+    }
+
+    ensurePlayerTexture (id, fallbackKey, color)
+    {
+        if (this.scene.textures.exists(id) && !this.scene.missingAssetIds?.has(id))
+        {
+            return id;
+        }
+
+        if (!this.scene.textures.exists(fallbackKey))
+        {
+            const asset = ASSET_BY_ID[id];
+            const texture = this.scene.textures.createCanvas(fallbackKey, asset.w, asset.h);
+            const context = texture.getContext();
+            context.fillStyle = color;
+            context.fillRect(0, 0, asset.w, asset.h);
+            context.fillStyle = '#111111';
+            context.fillRect(8, 12, asset.w - 16, asset.h - 24);
+            texture.refresh();
+        }
+
+        return fallbackKey;
     }
 
     get body ()
@@ -42,6 +66,29 @@ export class Player
     setState (state)
     {
         this.state = state;
+        this.swapTextureForState();
+    }
+
+    swapTextureForState ()
+    {
+        const stateToId = {
+            run: 'player.run',
+            jump: 'player.jump',
+            hurt: 'player.hurt'
+        };
+
+        const id = stateToId[this.state] ?? 'player.run';
+        const texture = this.ensurePlayerTexture(id, `${id}-fallback`, '#4fd1c5');
+        this.sprite.setTexture(texture);
+        this.sprite.setDisplaySize(PLAYER_W, PLAYER_H);
+    }
+
+    update ()
+    {
+        if (!this.isFalling && this.sprite.body.blocked.down && this.state !== 'run')
+        {
+            this.setState('run');
+        }
     }
 
     fallIntoCrater (onComplete)
@@ -51,6 +98,7 @@ export class Player
             return;
         }
         this.isFalling = true;
+        this.setState('hurt');
         this.sprite.body.enable = false;
         this.sprite.body.setVelocity(0, 0);
         this.scene.tweens.add({
